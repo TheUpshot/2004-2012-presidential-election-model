10eb149b53d451eb2ab0f4050aac2414692d0455
Josh Katz <josh.katz@nytimes.com>
2016-01-13 10:37:05 -0500

    add models

--- R/run.R
+++ R/run.R
@@ -2,6 +2,7 @@ rm(list = ls())
 
 ### packages
 library(dplyr)
+library(ggplot2)
 library(magrittr)
 library(lme4)
 library(readr)
@@ -9,7 +10,7 @@ library(readr)
 ### functions
 source("R/functions.R")
 
-getFit <- function(year) {
+getFit <- function(year, force = F) {
 
   datasets <- c("acs", "cps", "polls")
 
@@ -19,14 +20,14 @@ getFit <- function(year) {
     setNames(datasets)
 
   ### load / fit models
-  model <- getModels(data, year)
+  model <- getModels(data, year, force)
 
   acs <- cbind(data$acs, sapply(model, predict, data$acs,
                                 allow.new.levels = T))
 
   ### predict initial support and turnout models
   fits <- acs %>%
-    mutate(adjust = 0) %>%
+    mutate(adjust = 0, year = year) %>%
 
     adjust("turnout.acs.cvap", "turnout", "Freq") %>%
     mutate(turnoutFinal = plogis(turnout + adjust) * Freq) %>%
@@ -38,24 +39,32 @@ getFit <- function(year) {
     adjust("demTwoPartyShare", "twoParty", "majorFinal") %>%
     mutate(demFinal = plogis(twoParty + adjust) * majorFinal,
            repFinal = majorFinal - demFinal) %>%
+
     ungroup %>%
     mutate_each(funs(divide_by(., turnoutFinal)),
                 demFinal, repFinal, minorFinal)
 
-  saveRDS(fits, sprintf("out/fits-%s.rds", year))
-
-  fits
 }
 
 # fit models
 fits <- c(2004, 2012) %>%
-  lapply(function(yr) mutate(getFit(yr), year = yr)) %>%
+  sapply(getFit) %>%
   bind_rows()
 
 # get summary stats
 summary <- fits %>%
   group_by(state, race_eth, year) %>%
   summarize(demVote = weighted.mean(demFinal, turnoutFinal),
-            repVote = weighted.mean(repFinal, turnoutFinal))
+            repVote = weighted.mean(repFinal, turnoutFinal),
+            pop = sum(Freq),
+            turnout = sum(turnoutFinal) / pop)
+
+# plot
+ggplot(summary, aes(x = demVote, y = turnout, color = race_eth,
+                    size = pop)) +
+  geom_point(alpha = .65) +
+  scale_size_area(max_size = 15) +
+  facet_grid(year ~ .) +
+  theme_bw()
 
 write_csv(summary, "out/summary.csv")
